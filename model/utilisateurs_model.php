62a162cdddad1f64cd4f306f8d5fc47dc8b277e9
LauU5 <laureen.dolon@gmail.com>
2026-06-09 16:31:17 +0200

    ajout de plus d'option pour utilisateur model

--- model/utilisateurs_model.php
+++ model/utilisateurs_model.php
@@ -30,14 +30,26 @@ function mettreAJourProfil($id_user, $pseudo, $tel) {
     ]);
 }
 
+// Récupère les infos du chef d'équipe, de son équipe et de sa réservation
 function recupererInfosUtilisateur($id_user) {
     global $bdd;
-    // Jointure pour récupérer le score de l'équipe du joueur
-    $req = $bdd->prepare("SELECT u.pseudo, u.telephone, e.score_obtenu 
+    $req = $bdd->prepare("SELECT u.pseudo, u.telephone, u.email, e.nom_equipe, e.score_obtenu, e.type_menu, e.options_accessibilite, e.nb_participants, s.date_session 
                           FROM utilisateurs u 
                           JOIN equipes e ON u.id_equipe = e.id_equipe 
+                          LEFT JOIN sessions s ON e.id_session = s.id_session
                           WHERE u.id_utilisateur = :id");
     $req->execute(['id' => $id_user]);
     return $req->fetch();
 }
-?>
+
+// Récupère la liste des autres participants de l'équipe
+function recupererMembresEquipeParUtilisateur($id_user) {
+    global $bdd;
+    $req = $bdd->prepare("SELECT m.nom, m.prenom, m.pseudo 
+                          FROM membres_equipe m
+                          JOIN equipes e ON m.id_equipe = e.id_equipe
+                          JOIN utilisateurs u ON e.id_equipe = u.id_equipe
+                          WHERE u.id_utilisateur = :id");
+    $req->execute(['id' => $id_user]);
+    return $req->fetchAll();
+}
