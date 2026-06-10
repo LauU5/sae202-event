78037a7f4c83fa001f6d959a5355f15a87ec7992
LauU5 <laureen.dolon@gmail.com>
2026-06-10 21:36:23 +0200

    mdp

--- model/utilisateurs_model.php
+++ model/utilisateurs_model.php
@@ -1,6 +1,48 @@
-// ... (tes anciennes fonctions comme recupererInfosUtilisateur, etc.)
+<?php
+require_once 'conf/conf.inc.php';
 
-// Met à jour les infos complètes du capitaine
+function creerUtilisateur($id_equipe, $pseudo, $email, $mdp_hash, $tel) {
+    global $bdd;
+    $req = $bdd->prepare("INSERT INTO utilisateurs (id_equipe, pseudo, email, mot_de_passe, telephone) VALUES (:id_eq, :pseudo, :email, :mdp, :tel)");
+    return $req->execute([
+        'id_eq' => $id_equipe,
+        'pseudo' => $pseudo,
+        'email' => $email,
+        'mdp' => $mdp_hash,
+        'tel' => $tel
+    ]);
+}
+
+function verifierUtilisateur($email) {
+    global $bdd;
+    $req = $bdd->prepare("SELECT id_utilisateur, mot_de_passe, pseudo FROM utilisateurs WHERE email = :email");
+    $req->execute(['email' => $email]);
+    return $req->fetch();
+}
+
+function mettreAJourProfil($id_user, $pseudo, $tel) {
+    global $bdd;
+    $req = $bdd->prepare("UPDATE utilisateurs SET pseudo = :pseudo, telephone = :tel WHERE id_utilisateur = :id");
+    return $req->execute([
+        'pseudo' => $pseudo,
+        'tel' => $tel,
+        'id' => $id_user
+    ]);
+}
+
+function recupererInfosUtilisateur($id_user) {
+    global $bdd;
+    
+    $req = $bdd->prepare("SELECT u.pseudo, u.telephone, e.score_obtenu 
+                          FROM utilisateurs u 
+                          JOIN equipes e ON u.id_equipe = e.id_equipe 
+                          WHERE u.id_utilisateur = :id");
+    $req->execute(['id' => $id_user]);
+    return $req->fetch();
+}
+
+
+
 function mettreAJourProfilComplet($id, $nom, $prenom, $pseudo, $tel, $email) {
     global $bdd;
     $req = $bdd->prepare("UPDATE utilisateurs SET pseudo = :ps, telephone = :tel, email = :em, nom = :nom, prenom = :pre WHERE id_utilisateur = :id");
@@ -14,7 +56,7 @@ function mettreAJourProfilComplet($id, $nom, $prenom, $pseudo, $tel, $email) {
     ]);
 }
 
-// Met à jour un coéquipier spécifique
+
 function modifierMembreEquipe($id_m, $nom, $prenom, $pseudo) {
     global $bdd;
     $req = $bdd->prepare("UPDATE membres_equipe SET nom = :nom, prenom = :pre, pseudo = :ps WHERE id_membre = :id");
@@ -26,7 +68,7 @@ function modifierMembreEquipe($id_m, $nom, $prenom, $pseudo) {
     ]);
 }
 
-// Ajoute un commentaire en attente
+
 function ajouterCommentaire($id_u, $contenu) {
     global $bdd;
     $req = $bdd->prepare("INSERT INTO commentaires (id_utilisateur, contenu, statut) VALUES (:id_u, :cont, 'en_attente')");
